81dcffc4b67e2b8645a44c84f3b1893b0e631d6e
virdausa <team.cobusi@gmail.com>
2025-04-01 14:14:39 +0000

    feat: testing basic api, works

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,14 @@ use Illuminate\Validation\ValidationException;
 
 // Login API
 Route::post('/login', function (Request $request) {
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-    ]);
+    try {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+    } catch (\Illuminate\Validation\ValidationException $e) {
+        return response()->json(['message' => $e->validator->errors()->first()], 400);
+    }
 
     $user = User::where('email', $request->email)->first();
 
@@ -43,17 +47,27 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // Register API
 Route::post('/register', function (Request $request) {
-    $request->validate([
-        'name' => 'required|string|max:255',
-        'email' => 'required|email|unique:users',
-        'password' => 'required|min:6',
-    ]);
+    //return response()->json($request->all());
 
-    $user = User::create([
-        'name' => $request->name,
-        'email' => $request->email,
-        'password' => Hash::make($request->password),
-    ]);
+    try {
+        $validated = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+    } catch (\Illuminate\Validation\ValidationException $e) {
+        return response()->json(['message' => $e->validator->errors()->first()], 400);
+    }
+
+    try {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+    } catch (\Illuminate\Database\QueryException $e) {
+        return response()->json(['message' => 'User already registered'], 409);
+    }
 
     return response()->json([
         'token' => $user->createToken('auth-token')->plainTextToken
